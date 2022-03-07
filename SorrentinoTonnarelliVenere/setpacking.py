f730eb7b8119be042fb5509f005f18f976fb2e8d
mar-ven <marcovenere99@outlook.com>
2022-03-07 19:21:10 +0100

    Added reading from file input and usage of DWaveSampler

--- SorrentinoTonnarelliVenere/setpacking.py
+++ SorrentinoTonnarelliVenere/setpacking.py
@@ -1,5 +1,8 @@
 from dimod import BinaryQuadraticModel
-from dwave.system import LeapHybridSampler
+from dwave.system import DWaveSampler
+from dwave.system import EmbeddingComposite
+from dwave.inspector import *
+import json
 
 class SetPackingProblem:
     """This class represents a set-packing problem, 
@@ -14,7 +17,7 @@ class SetPackingProblem:
         weights: weight associated to each different subset.
             Defaults weights are unitary for each subset.
         constraints: a list of sets_number constraints.""" 
-        self.n = subsets 
+        self.s = subsets 
         if weights == None:
             weights = [1] * len(subsets)
         self.w = weights
@@ -25,17 +28,63 @@ class SetPackingProblem:
             Returns a dictionary in which subsets are the key of the dictionary, and their
             corresponding value is 1 if that subset is selected, 0 otherwise."""
         bqm = BinaryQuadraticModel({}, {}, 0, 'BINARY')
-        for i in range(len(self.n)):
+        for i in range(len(self.s)):
             bqm.offset += 1
-            bqm.add_variable(self.n[i], 0-(self.w[i])) #add variable for subset
+            bqm.add_variable(self.s[i], 0-(self.w[i])) #add variable for subset
         for cons in self.c:
             for i in range(len(cons)):
                 for j in range(i):
                     bqm.add_interaction(cons[i], cons[j], 6) #add constraint to avoid two non-disjoint subsets being selected       
-        sampler = LeapHybridSampler()
+        sampler = EmbeddingComposite(DWaveSampler())
         sampleset = sampler.sample(bqm, label="Set Packing")
+        #show(sampleset) 
         sample = sampleset.first.sample
         return sample
+
+
+def read_sanitized_file(filename):
+    """
+    This method receives input from a file, verifies if it is consistent and constructs a list of instances of the class SetPackingProblem.
+    Returns a list of instances of the class SetPackingProblem, if problems are correctly inserted.
+    Throws a ValueError exception if data type is not consistent.
+    Throws a json.decoder.JSONDecodeError exception if file is not correctly encoded.
+    """
+    with open(filename, "r") as f:
+        data = json.load(f)
+        problems = []
+        for problem in data:
+            subsets = problem["subsets"]
+            sets = []
+            weights = []
+            for set in subsets:
+                sets.append(set["name"])
+                if "weight" in set:
+                    try:
+                        weights.append(int(set["weight"]))
+                    except ValueError:
+                        print("Weight attribute must be integer")
+                        exit()
+                else:
+                    weights.append(1)
+                for i in range(len(sets)):
+                        for j in range(i):
+                            if(sets[i] == sets[j]):
+                                print("Identifier already inserted in subsets list")
+                                exit()
+            constraints = problem["constraints"]
+            c = []
+            for cons in constraints:
+                c.append(cons["sets"])
+                if not cons["sets"] <= sets:
+                    print("Undefined subset")
+                    exit()
+                for i in range(len(cons["sets"])):
+                    for j in range(i):
+                        if(cons["sets"][i] == cons["sets"][j]):
+                            print("Identifier already inserted in this constraint")
+                            exit()
+        problems.append(SetPackingProblem(sets, weights, c))
+    return problems
 
 def get_sanitized_input():
     """This method receives input from the user, verifies if it is consistent and constructs an instance of the class SetPackingProblem.
@@ -110,8 +159,13 @@ def get_sanitized_input():
             else:
                 print(e)
             continue
+problem = read_sanitized_file('SorrentinoTonnarelliVenere/data.json')[0]
+
+print(problem.solve())
+"""
 #create problem instance by reading user input
 problem = get_sanitized_input()
 
 #compute problem solution and show it
 print(problem.solve())
+"""
