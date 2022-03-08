0258bef4e91cca09d63f852c26dc78fd6995ae5b
mar-ven <marcovenere99@outlook.com>
2022-03-08 17:29:45 +0100

    Update and rename GeneratoreJSON.py to JSONGenerator.py

--- SorrentinoTonnarelliVenere/JSONGenerator.py
+++ SorrentinoTonnarelliVenere/JSONGenerator.py
@@ -20,41 +20,41 @@ def new_dict_without_weight(string):
     return dict(name=string)
       
 def new_cons(array,dim): 
-    m = random.randint(1,dim)
+    m = random.randint(2,dim)
     var = set()
     while len(var) < m:
         var.add(array[random.randint(0,len(array)-1)])
     return dict(sets = list(var))
  
 
+def generate(filename, n):
+    s = set()
+    while len(s) < n:
+        s.add(generate_random_string())
+    s = list(s)
+    """
+    We have to declare a list of n dictionaries in order to parse it in a
+    json array of objects
+    """
 
-   
-n = random.randint(1, 10)
-s = []
-for k in range (1,n):
-    s.append(generate_random_string())
-"""
-We have to declare a list of n dictionaries in order to parse it in a
-json array of objects
-"""
-
-sub = []
-for i in range(1,n):
-    if random.randint(0,1) == 1:
-        sub.append(new_dict_without_weight(s[i-1]))
-    else:
-        sub.append(new_dict_with_weight(s[i-1]))
+    sub = []
+    for i in range(1, n + 1):
+        if random.randint(0,1) == 1:
+            sub.append(new_dict_without_weight(s[i-1]))
+        else:
+            sub.append(new_dict_with_weight(s[i-1]))
+            
         
-    
-cons = []
-for j in range(1,n):
-    cons.append(new_cons(s,n))
+    cons = []
+    num = random.randint(1, n)
+    for j in range(1,num):
+        cons.append(new_cons(s,n))
 
-problem = []
-problem.append(dict(subsets=sub,constraints=cons))
+    problem = []
+    problem.append(dict(subsets=sub,constraints=cons))
 
-with open("sample.json", "w") as outfile:
-    json.dump(problem, outfile)
+    with open(filename, "w") as outfile:
+        json.dump(problem, outfile)
     
 
 
